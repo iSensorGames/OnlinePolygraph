d85c7b204706d53ba30c0d6a5531e85f46634048
isensor <isensor@dhcp129d238.cci.fsu.edu>
2019-09-11 14:14:10 -0400

    Backend: Split client and server setup.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const app = express();
-const PORT = 5000;
+const path = require('path');
 
-// routes
-app.get('/', (req, res) => {
-	res.send('Hello World from the iStudio machine');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve static files from the React app 
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// An API endpoint that returns a short list of items  
+app.get('/api/getList', (req, res) => {
+	let list = ['player1', 'player2', 'admin'];
+	res.json(list);	
 });
 
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+});
+
+// Server
 app.listen(PORT, () => {
 	console.log('Server listening to Port: ' + PORT);
 });
