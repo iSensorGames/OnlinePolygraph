4a7371eef3602ec679d38b768bf73960efee0e0a
Giwoo G Lee <gglee89@gmail.com>
2019-09-20 15:44:59 -0400

    Frontend: Chat UI Integration.

---
+++ frontend/src/pages/Game/discussions.js
@@ -0,0 +1,86 @@
+import avatarMale1 from "../../static/img/avatars/avatar-male-1.jpg";
+import avatarMale2 from "../../static/img/avatars/avatar-male-2.jpg";
+import avatarMale3 from "../../static/img/avatars/avatar-male-3.jpg";
+import avatarMale4 from "../../static/img/avatars/avatar-male-4.jpg";
+import avatarMale5 from "../../static/img/avatars/avatar-male-5.jpg";
+
+import avatarGroup1 from "../../static/img/avatars/avatar-group-1.jpg";
+import avatarGroup2 from "../../static/img/avatars/avatar-group-2.jpg";
+import avatarGroup3 from "../../static/img/avatars/avatar-group-3.jpg";
+import avatarGroup4 from "../../static/img/avatars/avatar-group-4.jpg";
+import avatarGroup5 from "../../static/img/avatars/avatar-group-5.jpg";
+
+export default [
+  {
+    name: "Ham Chuwon",
+    type: "direct",
+    img: avatarMale1,
+    lastText: "Please review and sign the binding agreement.",
+    messageSentDate: "Today",
+    status: "online",
+    isActive: true
+  },
+  {
+    name: "Quincy Hensen",
+    type: "direct",
+    img: avatarMale2,
+    lastText: "Additional information about the previous clients.",
+    messageSentDate: "Today"
+  },
+  {
+    name: "Mark Hog",
+    type: "direct",
+    img: avatarMale3,
+    lastText: "I'm looking to get a quote for the move from LA to NY.",
+    messageSentDate: "Feb 13"
+  },
+  {
+    name: "Sanne Viscaal",
+    type: "direct",
+    img: avatarMale4,
+    lastText: "My delivery address is pending final confirmation.",
+    messageSentDate: "Jan 10"
+  },
+  {
+    name: "Alex Just",
+    type: "direct",
+    img: avatarMale5,
+    lastText: "Sending all the requested insurance documents.",
+    messageSentDate: "May 2"
+  },
+  {
+    name: "The Musketeers",
+    type: "groups",
+    img: avatarGroup1,
+    lastText: "Please review and sign the binding agreement.",
+    messageSentDate: "Today"
+  },
+  {
+    name: "Watts Up",
+    type: "groups",
+    img: avatarGroup2,
+    lastText: "Additional information about the previous clients.",
+    messageSentDate: "Today"
+  },
+  {
+    name: "Memes",
+    type: "groups",
+    img: avatarGroup3,
+    lastText: "I'm looking to get a quote for the move from LA to NY.",
+    messageSentDate: "Feb 23"
+  },
+  {
+    name: "Supernovas",
+    type: "groups",
+    img: avatarGroup4,
+    lastText: "My delivery address is pending final confirmation.",
+    messageSentDate: "Feb 23"
+  },
+  {
+    name: "Squad Ghouls",
+    type: "groups",
+    img: avatarGroup5,
+    lastText: "Sending all the requested insurance documents.",
+    messageSentDate: "Feb 23"
+  }
+];
